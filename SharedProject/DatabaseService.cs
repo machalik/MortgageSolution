ee708f53c7e4d4f7de22346b60da6b17c5b78ac9
Erik Král <ekral@utb.cz>
2022-10-10 08:13:17 +0200

    Add project files.

---
+++ SharedProject/DatabaseService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedProject
+{
+    public class DatabaseService
+    {
+        public Model GetById(int id)
+        {
+            throw  new NotImplementedException();
+        }
+
+        public void Insert(Model model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Model model) 
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Model model) 
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SharedProject/Model.cs
@@ -0,0 +1,10 @@
+﻿namespace SharedProject
+{
+    public class Model
+    {
+        public int Id { get; set; }
+        public double LoanAmount { get; set; }
+        public double InterestRate { get; set; }
+        public int LoanTerm { get; set; }
+    }
+}
